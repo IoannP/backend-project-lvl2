269eae3af43b5b9c7c3615b23bb2b58c99d6b804
ioann <giohann>
2020-04-24 00:28:06 +0300

    refact plain.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -6,30 +6,28 @@ const stringify = (value) => {
   return `'{ ${getPairs.map((pair) => pair.join(': ')).join(', ')} }'`;
 };
 
-const plain = (arr, parent = '') => {
-  return arr
-    .reduce((acc, {
-      name,
-      state,
-      value,
-      changedValue,
-      children,
-    }) => {
-      if (children) {
-        return [...acc, plain(children, `${parent}${name}.`)];
-      }
-      if (state === 'changed') {
-        return [...acc, `Property '${parent}${name}' was changed from ${stringify(value)} to ${stringify(changedValue)}`];
-      }
-      if (state === 'deleted') {
-        return [...acc, `Property '${parent}${name}' was deleted`];
-      }
-      if (state === 'added') {
-        return [...acc, `Property '${parent}${name}' was added with value: ${stringify(value)}`];
-      }
-      return acc;
-    }, [])
-    .join('\n');
-};
+const plain = (arr, parent = '') => arr
+  .reduce((acc, {
+    name,
+    state,
+    value,
+    changedValue,
+    children,
+  }) => {
+    if (children) {
+      return [...acc, plain(children, `${parent}${name}.`)];
+    }
+    if (state === 'changed') {
+      return [...acc, `Property '${parent}${name}' was changed from ${stringify(value)} to ${stringify(changedValue)}`];
+    }
+    if (state === 'deleted') {
+      return [...acc, `Property '${parent}${name}' was deleted`];
+    }
+    if (state === 'added') {
+      return [...acc, `Property '${parent}${name}' was added with value: ${stringify(value)}`];
+    }
+    return acc;
+  }, [])
+  .join('\n');
 
 export default plain;
